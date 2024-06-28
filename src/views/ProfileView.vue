9171a0c0ee6861ad8f44968244e9dc5cc035416a
KimBergstroem <kimmen_b@hotmail.com>
2024-06-28 19:45:31 +0200

    style: profileView page and intregrated getprofile function #3 #6

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,164 @@
-<script setup></script>
-
 <template>
-  <div class="text-center mb-4">
-    <h1>Profile View</h1>
+  <div class="container">
+    <div v-if="isLoggedIn">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="text-center card-box">
+            <div class="member-card pt-2 pb-2">
+              <div class="thumb-lg member-thumb mx-auto">
+                <Avatar
+                  v-model:path="avatar_url"
+                  @upload="updateProfile"
+                  size="10" />
+              </div>
+              <div class="">
+                <h4>{{ username }}</h4>
+                <p class="text-muted">
+                  {{ work_title }} <span>| </span
+                  ><span
+                    ><a href="#" class="text-pink">{{ website }}</a></span
+                  >
+                </p>
+              </div>
+              <ul class="social-links list-inline">
+                <li class="list-inline-item">
+                  <a
+                    title=""
+                    data-placement="top"
+                    data-toggle="tooltip"
+                    class="tooltips"
+                    href=""
+                    data-original-title="Facebook"
+                    ><i class="fa fa-facebook"></i
+                  ></a>
+                </li>
+                <li class="list-inline-item">
+                  <a
+                    title=""
+                    data-placement="top"
+                    data-toggle="tooltip"
+                    class="tooltips"
+                    href=""
+                    data-original-title="Twitter"
+                    ><i class="fa fa-twitter"></i
+                  ></a>
+                </li>
+                <li class="list-inline-item">
+                  <a
+                    title=""
+                    data-placement="top"
+                    data-toggle="tooltip"
+                    class="tooltips"
+                    href=""
+                    data-original-title="Skype"
+                    ><i class="fa fa-skype"></i
+                  ></a>
+                </li>
+              </ul>
+              <button
+                type="button"
+                class="btn text-white btn-clr-primary mt-3 btn-rounded waves-effect w-md waves-light">
+                Create Task
+              </button>
+              <button
+                type="button"
+                class="btn text-white btn-clr-primary mt-3 btn-rounded waves-effect w-md waves-light">
+                Edit profile
+              </button>
+              <div class="mt-4">
+                <div class="row">
+                  <div class="col-4">
+                    <div class="mt-3">
+                      <h4>2563</h4>
+                      <p class="mb-0 text-muted">Pending</p>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="mt-3">
+                      <h4>6952</h4>
+                      <p class="mb-0 text-muted">Done</p>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="mt-3">
+                      <h4>1125</h4>
+                      <p class="mb-0 text-muted">Total</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <h2>Please log in, in order to see your profile</h2>
+    </div>
+    <div v-show="errorMsg">{{ errorMsg }}</div>
   </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import { useUserStore } from "../stores/userStore";
+import Avatar from "../components/profiles/ProfileAvatar.vue";
+
+const userStore = useUserStore();
+const isLoggedIn = computed(() => userStore.isLoggedIn);
+
+const loading = ref(false);
+const username = ref(null);
+const website = ref(null);
+const avatar_url = ref(null);
+const work_title = ref(null);
+
+onMounted(() => {
+  getProfile();
+});
+
+async function getProfile() {
+  try {
+    loading.value = true;
+    await userStore.fetchUser();
+    username.value = userStore.profile.username;
+    website.value = userStore.profile.website;
+    work_title.value = userStore.profile.work_title;
+    avatar_url.value = userStore.profile.avatar_url;
+  } catch (error) {
+    console.error("Error fetching profile:", error.message);
+  } finally {
+    loading.value = false;
+  }
+}
+
+async function updateProfile() {
+  try {
+    loading.value = true;
+    const updates = {
+      id: userStore.user.id,
+      username: username.value,
+      website: website.value,
+      avatar_url: avatar_url.value,
+      work_title: work_title.value,
+      updated_at: new Date(),
+    };
+
+    console.log("Update Payload:", updates); // Log the payload
+
+    await userStore.updateProfile(updates);
+
+    console.log("Profile updated successfully"); // Log the response
+  } catch (error) {
+    console.error("Error updating profile:", error.message);
+  } finally {
+    loading.value = false;
+  }
+}
+</script>
+
+<style>
+h2 {
+  text-align: center;
+}
+</style>
