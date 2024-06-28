9d47fee7d8cf70ccec7dea85444dd1fb41edfd21
KimBergstroem <kimmen_b@hotmail.com>
2024-06-28 16:22:21 +0200

    style: signIn view and integrated sign-in functionality #3 #7

--- src/components/authentication/SignIn.vue
+++ src/components/authentication/SignIn.vue
@@ -1,21 +1,109 @@
 <template>
-  <h1>SignIn Log In</h1>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card-group mb-0">
+          <div class="card p-4 bg-darken">
+            <div class="card-body text-white">
+              <h1>Login</h1>
+              <p class="text-muted">Sign In to your account</p>
+              <form @submit.prevent="signIn" class="form-sign-in">
+                <div class="input-group mb-3">
+                  <span class="input-group-addon"
+                    ><i class="fa fa-user"></i
+                  ></span>
+                  <input
+                    type="email"
+                    class="input-field form-control"
+                    placeholder="example@gmail.com"
+                    id="email"
+                    v-model="email"
+                    required />
+                </div>
+                <div class="input-group mb-4">
+                  <span class="input-group-addon"
+                    ><i class="fa fa-lock"></i
+                  ></span>
+                  <input
+                    type="password"
+                    class="input-field form-control"
+                    placeholder="**********"
+                    id="password"
+                    v-model="password"
+                    required />
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    <button
+                      class="button text-white btn btn-clr-primary mb-2"
+                      type="submit">
+                      Enter
+                    </button>
+                  </div>
+                  <div class="col-6 text-right">
+                    <button type="button" class="btn btn-link px-0 text-muted">
+                      Forgot password?
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="card text-white btn-clr-primary py-5 d-md-down-none">
+            <div class="card-body text-center">
+              <div>
+                <h2>Sign up</h2>
+                <p>
+                  What are you waiting for, sign up today and start create ne
+                  reminders tasks for easily manage your daily life.
+                </p>
 
-  <p class="mt-4">
-    Don't have an account?
-    <PersonalRouter
-      :route="route"
-      :buttonText="buttonText"
-      class="sign-up-link" />
-  </p>
+                <p>Don't have an account?</p>
+                <p>
+                  <PersonalRouter
+                    :route="route"
+                    :buttonText="buttonText"
+                    class="sign-up-link" />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-show="errorMsg">{{ errorMsg }}</div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { supabase } from "../../supabase/supabase";
+import { useUserStore } from "../../stores/userStore";
 import PersonalRouter from "./PersonalRouter.vue";
 
-// Route Variables
 const route = "/auth/signup";
 const buttonText = "Sign Up";
+
+const errorMsg = ref("");
+const redirect = useRouter();
+
+const signIn = async () => {
+  if (password.value) {
+    try {
+      await useUserStore().signIn(email.value, password.value);
+      redirect.push({ path: "/" });
+      console.log("Logged in successfully");
+    } catch (error) {
+      errorMsg.value = error.message;
+      setTimeout(() => {
+        errorMsg.value = "";
+      }, 5000);
+    }
+    return;
+  }
+  errorMsg.value = "error";
+};
 </script>
 
 <style scoped></style>
