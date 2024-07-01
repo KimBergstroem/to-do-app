d39817a5cff351aa6f8b58b2df3cdb0372e3c0e0
KimBergstroem <kimmen_b@hotmail.com>
2024-07-01 20:15:08 +0200

    feat: installed fontawesome lib for import in main.js

--- src/main.js
+++ src/main.js
@@ -8,9 +8,21 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faEdit,
+  faTrashAlt,
+  faUser,
+  faLaptopFile,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEdit, faTrashAlt, faUser, faLaptopFile);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
