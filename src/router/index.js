34ee99478949294dc349e95b0c2c46a2dcf1e291
KimBergstroem <kimmen_b@hotmail.com>
2024-07-03 16:13:57 +0200

    add: meta 'title' on each component view

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,90 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProfileView from "../views/ProfileView.vue";
+import ProfileEdit from "../components/profiles/ProfileEdit.vue";
+import TaskView from "../views/TaskView.vue";
+import TaskDetailView from "../views/TaskDetailView.vue";
+import TaskCreate from "../components/tasks/TaskCreate.vue";
 import Auth from "../components/authentication/Auth.vue";
 import SignIn from "../components/authentication/SignIn.vue";
 import SignUp from "../components/authentication/SignUp.vue";
-import EditProfile from "../components/profiles/EditProfile.vue";
 
 const routes = [
   {
     path: "/auth",
     component: Auth,
     children: [
-      { path: "signin", component: SignIn },
-      { path: "signup", component: SignUp },
+      {
+        path: "signin",
+        component: SignIn,
+        meta: {
+          title: "Sign In",
+        },
+      },
+      {
+        path: "signup",
+        component: SignUp,
+        meta: {
+          title: "Sign Up",
+        },
+      },
     ],
   },
-  { path: "/", component: HomeView },
-  { path: "/profile", component: ProfileView },
-  { path: "/profile/edit", component: EditProfile },
+  {
+    path: "/",
+    component: HomeView,
+    meta: {
+      title: "Home",
+    },
+  },
+  {
+    path: "/profile",
+    component: ProfileView,
+    meta: {
+      title: "Profile",
+    },
+  },
+  {
+    path: "/profile/edit",
+    component: ProfileEdit,
+    meta: {
+      title: "Edit Profile",
+    },
+  },
+  {
+    path: "/tasks",
+    component: TaskView,
+    meta: {
+      title: "Tasks",
+    },
+  },
+  {
+    path: "/create",
+    name: "TaskCreate",
+    component: TaskCreate,
+    meta: {
+      title: "Create Task",
+    },
+  },
+  {
+    path: "/view-todo/:todoId",
+    name: "View-todo",
+    component: TaskDetailView,
+    meta: {
+      title: "Task Details",
+    },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Set browser title for each route
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Reminder To Do`;
+  next();
 });
 
 export default router;
