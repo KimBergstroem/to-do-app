b11de8b86a3c5fc281b03e9b21e0092cc4adbe90
KimBergstroem <kimmen_b@hotmail.com>
2024-07-03 16:18:34 +0200

    add: taskview component with the nessacary fetch from taskstore to view tasks

---
+++ src/views/TaskView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div v-if="isLoggedIn">
+      <div v-if="taskStore.dataLoaded">
+        <!-- No Data -->
+        <div
+          v-if="taskStore.tasks.length === 0"
+          class="w-100 d-flex flex-column align-items-center">
+          <h1 class="text-2xl">Looks empty here...</h1>
+          <router-link class="btn text-white btn-clr-primary mt-3" to="/create"
+            >Create Task</router-link
+          >
+        </div>
+
+        <!-- Data cards -->
+        <div
+          class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+          <div class="col" v-for="todo in taskStore.tasks" :key="todo.id">
+            <router-link
+              :to="{ name: 'View-todo', params: { todoId: todo.id } }"
+              class="card shadow-sm text-decoration-none text-dark"
+              :style="{ backgroundColor: todo.backgroundColor }">
+              <!-- Logo based on Task type -->
+              <h1 class="card-img-top text-center py-3">
+                <font-awesome-icon
+                  v-if="todo.todosType === 'personal'"
+                  icon="user"
+                  class="fa-solid fa-user fa-2x" />
+                <font-awesome-icon
+                  v-if="todo.todosType === 'work'"
+                  icon="laptop-file"
+                  class="fa-2x" />
+              </h1>
+              <div class="card-body d-flex flex-column align-items-center">
+                <p
+                  class="badge bg-white text-black px-3 py-2 rounded-pill mb-3">
+                  {{ todo.todosType }}
+                </p>
+                <h5 class="card-title text-at-light-green">
+                  {{ todo.todosName }}
+                </h5>
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div v-else class="container mt-10">
+        <p>Loading data...</p>
+      </div>
+    </div>
+    <div v-else class="d-flex flex-column align-items-center">
+      <h1 class="text-2xl">Please log in before you can manage your tasks.</h1>
+      <router-link class="btn text-white btn-clr-primary mt-3" to="/auth/signIn"
+        >Sign In</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { useTaskStore } from "../stores/taskStore";
+import { useUserStore } from "../stores/userStore";
+
+const taskStore = useTaskStore();
+const userStore = useUserStore();
+
+const isLoggedIn = userStore.isLoggedIn;
+
+onMounted(() => {
+  if (isLoggedIn) {
+    taskStore.fetchTasks();
+  }
+});
+</script>
