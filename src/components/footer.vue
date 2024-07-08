235cb0a08676611b6ef185119aeaa4315cc46470
KimBergstroem <kimmen_b@hotmail.com>
2024-07-08 17:40:03 +0200

    add: author and repo links in footer section

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,35 @@
 <template>
   <div>
     <p>&copy; 2024 Reminder To Do. All rights reserved.</p>
+
+    <p>
+      <a href="https://github.com/KimBergstroem" target="_blank" class="small"
+        >Author</a
+      >
+      -
+      <a
+        href="https://github.com/KimBergstroem/to-do-app"
+        target="_blank"
+        class="small"
+        >Github</a
+      >
+    </p>
+
     <p class="small">Educational purpose only</p>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+  color: rgb(161, 161, 161);
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  color: rgb(161, 161, 161);
+}
+p + p {
+  margin-top: 2px;
+}
+</style>
