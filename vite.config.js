3fea62b3bf7ccbd5831544ff6bf5b76c72ff7c8c
KimBergstroem <kimmen_b@hotmail.com>
2024-07-10 16:50:17 +0200

    chore: enable css minification in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +8,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require("postcss-clean")()],
     },
   },
 });
