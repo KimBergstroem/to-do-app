09533142e01f69a75cf347344bcefdd8814ca197
KimBergstroem <kimmen_b@hotmail.com>
2024-07-10 16:59:50 +0200

    fix: resolve Netlify build error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
+import path from "path";
+import postcssClean from "postcss-clean";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   css: {
     postcss: {
-      plugins: [require("postcss-clean")()],
+      plugins: [postcssClean()],
     },
   },
 });
