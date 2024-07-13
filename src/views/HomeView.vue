4b0a0f0f44639f5243ca4de20daf133664a525ff
Kim Bergstrom <kimmen_b@hotmail.com>
2024-07-13 09:01:14 +0000

    style: made the 'welcome section' cards display the same height besides the content.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,45 +5,45 @@
         <div class="container">
           <h2 class="text-center mb-4">Welcome to Your Dashboard!</h2>
 
-          <div class="row justify-content-center">
-            <div class="col-md-4 mb-4">
-              <div class="card">
-                <div class="card-body">
+          <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center align-items-stretch">
+            <div class="col mb-4 d-flex">
+              <div class="card flex-fill">
+                <div class="card-body d-flex flex-column">
                   <h3 class="card-title">Create New Task</h3>
-                  <p class="card-text">
+                  <p class="card-text flex-grow-1">
                     Start organizing your tasks efficiently.
                   </p>
                   <router-link
                     to="/create"
-                    class="btn text-white btn-clr-primary btn-block"
+                    class="btn text-white btn-clr-primary btn-block mt-auto"
                     >Create Task</router-link
                   >
                 </div>
               </div>
             </div>
 
-            <div class="col-md-4 mb-4">
-              <div class="card">
-                <div class="card-body">
+            <div class="col mb-4 d-flex">
+              <div class="card flex-fill">
+                <div class="card-body d-flex flex-column">
                   <h3 class="card-title">Go to Your Profile</h3>
-                  <p class="card-text">
+                  <p class="card-text flex-grow-1">
                     View and edit your profile information.
                   </p>
                   <router-link
                     to="/profile"
-                    class="btn text-white btn-clr-primary btn-block"
+                    class="btn text-white btn-clr-primary btn-block mt-auto"
                     >Go to Profile</router-link
                   >
                 </div>
               </div>
             </div>
 
-            <div class="col-md-4 mb-4">
-              <div class="card">
-                <div class="card-body">
+            <div class="col mb-4 d-flex">
+              <div class="card flex-fill">
+                <div class="card-body d-flex flex-column">
                   <h3 class="card-title">More Information</h3>
-                  <p class="card-text">Learn more about our capabilities.👍</p>
-                  <button class="btn text-white btn-clr-primary btn-block">
+                  <p class="card-text flex-grow-1">Learn more about our capabilities.👍</p>
+                  <button class="btn text-white btn-clr-primary btn-block mt-auto">
                     Learn More
                   </button>
                 </div>
